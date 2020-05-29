b545a88d6993956f8cf735c31d77fbe35832f06a
Carl Lerche <me@carllerche.com>
2020-05-28 21:27:45 -0700

    better responsive floaty things

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,30 +11,35 @@ const icons = [
   "tower",
   "tracing",
 ].map((id) => (
-  <div key={id} id={`tk-float-${id}`} className="tk-float"><img src={`/img/icons/${id}.svg`} /></div>
+  <div key={id} id={`tk-float-${id}`} className="tk-float">
+    <img src={`/img/icons/${id}.svg`} />
+  </div>
 ));
 
 const Hero: FC = () => (
   <section className="hero is-primary tk-intro">
     <div className="hero-body">
-      <div className="container has-text-centered tk-hero-bg">
+      <div className="container tk-hero-bg">
         {icons}
-        <h1 className="title">
-          Build reliable network applications without compromising speed.
-        </h1>
-        <h2 className="subtitle">
-          Tokio is an asynchronous runtime for the Rust programming language. It
-          provides the building blocks needed for writing network applications.
-          It gives the flexibility to target a wide range of systems, from large
-          servers with dozens of cores to small embedded devices.
-          {/* Tokio is an open source library providing an asynchronous, event
-                driven platform for building fast, reliable, and lightweight
-                network applications. It leverages Rust's ownership and
-                concurrency model to ensure thread safety. */}
-        </h2>
-        <a href={gettingStarted} className="button is-link is-medium">
-          Get Started
-        </a>
+        <div className="container has-text-centered">
+          <h1 className="title">
+            Build reliable network applications without compromising speed.
+          </h1>
+          <h2 className="subtitle">
+            Tokio is an asynchronous runtime for the Rust programming language.
+            It provides the building blocks needed for writing network
+            applications. It gives the flexibility to target a wide range of
+            systems, from large servers with dozens of cores to small embedded
+            devices.
+            {/* Tokio is an open source library providing an asynchronous, event
+                  driven platform for building fast, reliable, and lightweight
+                  network applications. It leverages Rust's ownership and
+                  concurrency model to ensure thread safety. */}
+          </h2>
+          <a href={gettingStarted} className="button is-link is-medium">
+            Get Started
+          </a>
+        </div>
       </div>
     </div>
   </section>
