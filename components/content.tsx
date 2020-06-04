ace25b9f69bf7978bc1d4a4357babd6e1da4ec83
Carl Lerche <me@carllerche.com>
2020-06-04 14:58:13 -0700

    stub page summary menu

--- components/content.tsx
+++ components/content.tsx
@@ -11,6 +11,59 @@ const CodeBlock = ({ language, value }) => {
   );
 };
 
+function Footer({ next, prev }) {
+  return (
+    <div className="tk-doc-footer">
+      <div className="level">
+        <div className="level-left">
+          <div className="level-item tk-prev">
+            {prev && (
+              <a href={prev.href}>
+                <span className="tk-arrow" style={{ marginRight: "0.5rem" }}>
+                  <img src="/img/arrow-left.svg" />
+                </span>
+                {prev.title}
+              </a>
+            )}
+          </div>
+        </div>
+        <div className="level-right">
+          <div className="level-item tk-next">
+            {next && (
+              <a href={next.href}>
+                {next.title}
+                <span className="tk-arrow" style={{ marginLeft: "0.5rem" }}>
+                  <img src="/img/arrow-right.svg" />
+                </span>
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="level">
+        <div className="level-left">
+          <div className="level-item tk-help-links">
+            <p>
+              Get Help:
+              <a href="https://github.com/tokio-rs/tokio/discussions">
+                <GitHubIcon className="is-medium" />
+              </a>
+              <a href="https://discord.gg/tokio">
+                <DiscordIcon className="is-medium" />
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="level-right">
+          <div className="level-item tk-edit-this-page">
+            <a href="#">Edit this page</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Content({ menu, href, title, next, prev, body }) {
   return (
     <>
@@ -18,69 +71,50 @@ export default function Content({ menu, href, title, next, prev, body }) {
         <div className="column is-one-quarter tk-docs-nav">
           <Menu href={href} menu={menu} />
         </div>
-        <div className="column is-three-quarters">
-          <section className="section content tk-content">
-            <h1 className="title">{title}</h1>
-            <ReactMarkdown
-              escapeHtml={false}
-              source={body}
-              renderers={{ code: CodeBlock }}
-            />
-            <div className="tk-doc-footer">
-              <div className="level">
-                <div className="level-left">
-                  <div className="level-item tk-prev">
-                    {prev && (
-                      <a href={prev.href}>
-                        <span
-                          className="tk-arrow"
-                          style={{ marginRight: "0.5rem" }}
-                        >
-                          <img src="/img/arrow-left.svg" />
-                        </span>
-                        {prev.title}
-                      </a>
-                    )}
-                  </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item tk-next">
-                    {next && (
-                      <a href={next.href}>
-                        {next.title}
-                        <span
-                          className="tk-arrow"
-                          style={{ marginLeft: "0.5rem" }}
-                        >
-                          <img src="/img/arrow-right.svg" />
-                        </span>
-                      </a>
-                    )}
-                  </div>
-                </div>
+        <div className="column is-three-quarters tk-content">
+          <section className="section content">
+            <div className="columns">
+              <div className="column tk-markdown">
+                <h1 className="title">{title}</h1>
+                <ReactMarkdown
+                  escapeHtml={false}
+                  source={body}
+                  renderers={{ code: CodeBlock }}
+                />
               </div>
-              <div className="level">
-                <div className="level-left">
-                  <div className="level-item tk-help-links">
-                    <p>
-                      Get Help:
-                      <a href="https://github.com/tokio-rs/tokio/discussions">
-                        <GitHubIcon className="is-medium" />
-                      </a>
-                      <a href="https://discord.gg/tokio">
-                        <DiscordIcon className="is-medium" />
-                      </a>
-                    </p>
-                  </div>
-                </div>
-                <div className="level-right">
-                  <div className="level-item tk-edit-this-page">
-                    <a href="#">Edit this page</a>
-                  </div>
-                </div>
-              </div>
+              <aside className="column is-one-third tk-content-summary">
+                <ul className="tk-content-summary-menu">
+                  <li>
+                    <a href="#">Motivation</a>
+                  </li>
+                  <li>
+                    <a href="#">Using tokio compat</a>
+                    <ul>
+                      <li>
+                        <a href="#">Getting Started</a>
+                      </li>
+                      <li>
+                        <a href="#">Notes</a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          hello world this is a long item that will wrap some
+                          and that is OK
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Case Study: Vector</a>
+                  </li>
+                  <li>
+                    <a href="#">Conclusion</a>
+                  </li>
+                </ul>
+              </aside>
             </div>
           </section>
+          <Footer next={next} prev={prev} />
         </div>
       </div>
     </>
